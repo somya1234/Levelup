0fe19fde438cf0c948c524e27e83e1cb9653dbdb
Somya <somya1234@gmail.com>
2020-08-26 00:44:04 +0530

    levelup

---
+++ rajneeshBhaiya/cycle2/GenericTree/l001/hw.md
@@ -0,0 +1,9 @@
+1. roottonode and lca 
+2. construct sir method 
+3. revise foundation GT.
+
+4. linearize 
+5. 103,114
+
+1. preorder ans postorder construct tree - bt - leetcode - 889
+2. validate bst
--- rajneeshBhaiya/cycle2/GenericTree/l001/l001.java
+++ rajneeshBhaiya/cycle2/GenericTree/l001/l001.java
@@ -34,6 +34,7 @@ public class l001 {
 /********************************************************************************************/
     public static void solve(){
         int[] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,120,-1,130,-1,-1,90,-1,-1,40,100,-1,110,-1,-1,-1};
+        // int[] arr = {10,20,-1,30,-1,40,-1,-1}; // for linearize 
         Node root = constructGT(arr);
         // display(root);
         // System.out.println(height(root));
@@ -41,7 +42,12 @@ public class l001 {
         // System.out.println(max(root));
         // System.out.println(find(root, 80));
         // zigZag(root);
-        System.out.println(isMirror(root,root));
+        // System.out.println(isMirror(root,root));
+        // linearize(root);
+        // linearize(root);
+        // display(root);
+        attachLeaves(root);
+        display(root);
     }
 
     /********************************************************************************************/
@@ -97,6 +103,45 @@ public class l001 {
     }
     /********************************************************************************************/
 
+    public static boolean nodetoRootPath(Node node, int data , ArrayList<Node> path){
+        if(node.data == data){
+            path.add(node);
+            return true;
+        }
+
+        boolean res = false;
+        for(Node child:node.children){
+            return res || nodetoRootPath(child, data, path);
+        }
+        if(res){
+            path.add(node);
+        }
+        return res;
+    }
+
+    public static void lca(Node node, int a, int b){
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        nodetoRootPath(node, a, path1);
+        nodetoRootPath(node, b, path2);
+
+        int i = path1.size()-1;
+        int j = path2.size()-1;
+
+        Node lca = null;
+        while(i>=0 && j>=0){
+            if(path1.get(i)!=path2.get(j)){
+                break;
+            }
+            lca = path1.get(i);
+            i--;
+            j--;
+        }
+
+        System.out.println(lca.data);
+    }
+
     public static void zigZag(Node node){
         int level = 0;
         LinkedList<Node> ms = new LinkedList<>();
@@ -121,8 +166,10 @@ public class l001 {
             if(ms.size()==0){
                 level++;
                 System.out.println();
+                //swap it 
+                LinkedList<Node> temp = ms;
                 ms = hs;
-                hs = new LinkedList<>();
+                hs = temp;
             }
             
         }
@@ -147,4 +194,92 @@ public class l001 {
     }
 
     /******************************************************************************************** */
+
+    //O(n2) approach 
+    public static void linearize(Node node){
+
+        for(int i= node.children.size()-1;i>=0;i--){
+            Node child = node.children.get(i);
+            if(child.children.size()>1){
+                linearize(child);
+            }
+        }
+
+        for(int i=node.children.size()-1;i>0;i--){
+            Node child = node.children.get(i);
+            node.children.remove(node.children.size()-1);
+            Node parent = node.children.get(node.children.size()-1);
+            Node parentKiTail = getTail(parent);
+            parentKiTail.children.add(child);
+        }
+    }
+
+    public static Node getTail(Node node){
+        while(node.children.size()>0){
+            node = node.children.get(0);
+        }
+        return node;
+    }
+
+    //=====
+    //O(n) approach 
+    public static Node linearize2(Node node){
+
+        //last node gets linearize gives tail
+        int n = node.children.size();
+        Node oTail =  linearize2(node.children.get(node.children.size()-1)); ;//overall tail 
+        for(int i=n-2;i>=0;i--){
+            //start from second last node
+            Node tail = linearize2(node.children.get(i));
+            tail.children.add(node.children.get(i+1));
+            node.children.remove(node.children.size()-1);
+        }
+        return oTail; //think like root will return its tail, which is tail of last children 
+    }
+
+
+    //company question -> 32 lpa
+    // public static Node attachLeaves(Node node){
+    //     if(node.children.size()==0){
+    //         return node;
+    //     }
+    //     Node tail = node;
+    //     while(node.children.size()==1){
+    //         node = node.children.get(0);
+    //     }
+    //     tail = node;
+    //     for(int i=0;i<node.children.size()-1;i++){
+    //         Node tail1 = attachLeaves(node.children.get(i));
+    //         Node tail2 = attachLeaves(node.children.get(i+1));
+    //         tail1.children.add(tail2);
+    //         tail2.children.add(tail1);
+    //         tail = tail2;
+    //     }
+    //     return tail;
+    // }
+
+
+    //binary tree
+    static Node prev = null;
+        public static void attachLeaves(Node node){
+            if(node == null){ return; }
+            if(node.left == null && node.right == null){
+                if(prev == null){
+                    node.left = prev;
+                    prev = node;
+                    return;
+                } else {
+                    node.left = prev;
+                    prev.right = node;
+                    prev = node;
+                    return;
+                }
+            }
+
+            attachLeaves(node.left);
+
+            attachLeaves(node.right);
+        }
+
+    /********************************************************************************************* */
 }
