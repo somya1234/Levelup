18f6343998b745467d927270d350e0b0f1f4920e
Somya <somya1234@gmail.com>
2020-09-25 00:05:21 +0530

    update

--- rajneeshBhaiya/cycle4/Graph/l003/l003.java
+++ rajneeshBhaiya/cycle4/Graph/l003/l003.java
@@ -58,7 +58,70 @@ public class l003 {
 
     }
 
-    
 
+    /************************************************************************************/
+
+    // union Find 
+
+    static int[] par; 
+    static int[] size; 
+
+    public static int findPar(int u ){
+        if(u == par[u]) return u ; 
+        // path compression 
+        return par[u] = findPar(par[u]); 
+    }
+
+    public static void merge(int p1, int p2){
+        if(size[p1]<size[p2]){
+            par[p1] = p2; 
+            size[p2] += size[p1]; 
+        } else {
+            par[p2] = p1; 
+            size[p1]+=size[p2];
+        }
+    }
+
+    // {{u,v,w}}
+    public static void unionFind(int[][] gp){
+        // new graph creation 
+        for(int i =0; i<N; i++) graph[i] = new ArrayList<>(); 
+
+        Arrays.sort(gp, (a,b)->{
+            // sort on basis of weight .
+            return a[2] - b[2]; // this- other // Increasing 
+            // return b[2] - a[2]; // other- this// reverse of default // decreasing 
+        })
+
+        // initially, all are their own parent.
+        for(int i =0; i<N; i++){
+            par[i] = i; 
+        }
+
+        for(int[] a : gp){
+            int u = a[0]; 
+            int v = a[1]; 
+            int w = a[2];
+
+            int p1 = findPar(u); 
+            int p2 = findPar(v);
+
+            if(p1!=p2){
+                // add edge in graph when their parent are not equal, otherwise cycle is forming.
+                addEdge(u,v,w);
+
+
+                // smaller size ko add krdo bigger me -> is merge fn 
+                merge(p1, p2);
+
+                // but let's not write that here 
+                // uske bina bhi kaam chl jayega 
+                // par[p1] = p2; 
+            }
+        }
+    }
+
+    
+    /************************************************************************************/
 
 }
--- rajneeshBhaiya/cycle4/Graph/pending.md
+++ rajneeshBhaiya/cycle4/Graph/pending.md
@@ -1,1 +1,7 @@
 l001, l002
+
+l003 - 20 sep - just see the end part video, do whole lecture.
+l004 - (21 sep) see the video  and do questions.
+l005 - 22 sep (just do qs of prims class and complete all codes ) // do everything.
+
+l006 - 24 sep - whole.
