f4640b4e8a5ee4702bda5c1668135853722fc783
Somya <somya1234@gmail.com>
2020-09-29 23:13:20 +0530

    update

--- rajneeshBhaiya/cycle3/DP2/l002/l002.java
+++ rajneeshBhaiya/cycle3/DP2/l002/l002.java
@@ -6,7 +6,12 @@ public class l002 {
     }
 
     public static void solve() {
-        solve1();
+        // LPS
+        // solve1();
+        // LCS
+        // solve2();
+        //count palindromic subsq
+        solve3();
     }
 
     /*********************************************************************************************** */
@@ -41,9 +46,9 @@ public class l002 {
         // but called in memo due to reference point is 0 or base condn 0
         // eg aaaa, extra call on 0.
 
-        //  if you fill -1.  then remember - (-1+2= 1)
+        // if you fill -1. then remember - (-1+2= 1)
         // for (int[] d : dp)
-        //     Arrays.fill(d, -1);
+        // Arrays.fill(d, -1);
         /*
          * -1 -1 -1 -1 -1 -1 -1 -1 4 4 5 5 5 -1 -1 2 2 2 2 2 2 -1 3 5 5 5 -1 0 1 1 1 1 1
          * 1 1 3 3 4 4 -1 -1 -1 1 1 1 1 1 1 1 -1 4 4 -1 -1 -1 -1 1 1 1 1 1 1 2 -1 4 -1
@@ -53,16 +58,17 @@ public class l002 {
          * -1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1
          */
 
-        //  memoization 
+        // memoization
         // System.out.println(longestPlaindromicSubsq_Mem(0, n - 1, str, dp));
         // System.out.println(longestPlaindromicSubsq_Tab(0, n-1, str, dp));
         // print2d(dp);
 
-        // display the longest palindromic subsequence string 
-        int len = longestPlaindromicSubsq_Mem(0, n-1, str, dp);
-        char[] ans = new char[len];
-        System.out.println(len);
-        printLPS(str, 0,n-1, dp, ans, 0, len-1);
+        // display the longest palindromic subsequence string
+        // int len = longestPlaindromicSubsq_Mem(0, n - 1, str, dp);
+        // char[] ans = new char[len];
+
+        // printLPS(str, 0, n - 1, dp, ans, 0, len - 1);
+        // printALL_LPS(str, 0, n - 1, dp, ans, 0, len - 1);
     }
 
     public static int longestPlaindromicSubsq_Rec(int si, int ei, String str) {
@@ -102,7 +108,7 @@ public class l002 {
         int n = str.length();
         for (int diag = 0; diag < n; diag++) {
             for (si = 0, ei = diag; ei < n; si++, ei++) {
-                if (si == ei){
+                if (si == ei) {
                     dp[si][ei] = 1;
                     continue;
                 }
@@ -110,43 +116,164 @@ public class l002 {
                 if (str.charAt(si) == str.charAt(ei))
                     dp[si][ei] = dp[si + 1][ei - 1] + 2;
                 else
-                     dp[si][ei] = Math.max(dp[si][ ei - 1],dp[si + 1][ ei]);
+                    dp[si][ei] = Math.max(dp[si][ei - 1], dp[si + 1][ei]);
             }
         }
-        return dp[0][n-1];
+        return dp[0][n - 1];
     }
 
     /************************************************************************************/
     // print longest plindromic subsequence
 
     // i and j for char[]
-    // si and ei for string 
+    // si and ei for string
     // It gives single longest palindromic subsq.
-    public static void printLPS(String str, int si, int ei, int[][] dp, char[] ans, int i, int j ){
-        if(si>=ei) {
-            if(si==ei)
+    public static void printLPS(String str, int si, int ei, int[][] dp, char[] ans, int i, int j) {
+        if (si >= ei) {
+            if (si == ei)
                 ans[i] = str.charAt(si);
-            for(char ch : ans) System.out.print(ch);
+            for (char ch : ans)
+                System.out.print(ch);
             System.out.println();
             return;
         }
 
-    
         // if(si>ei) return;
 
-        if(str.charAt(si)==str.charAt(ei)){
+        if (str.charAt(si) == str.charAt(ei)) {
             ans[i] = ans[j] = str.charAt(si);
-            printLPS(str, si+1, ei-1, dp, ans, i+1, j-1);
-        } else if(dp[si+1][ei]>dp[si][ei-1]){
+            printLPS(str, si + 1, ei - 1, dp, ans, i + 1, j - 1);
+        } else if (dp[si + 1][ei] > dp[si][ei - 1]) {
             // because, here we only took max out of two, ans is not formed here
             // so can't add anything in our char[] ans.
-            printLPS(str, si+1, ei, dp, ans, i, j);
-        } else{
-            printLPS(str, si, ei-1, dp, ans, i, j );
+            printLPS(str, si + 1, ei, dp, ans, i, j);
+        } else {
+            printLPS(str, si, ei - 1, dp, ans, i, j);
         }
     }
 
+    public static void printALL_LPS(String str, int si, int ei, int[][] dp, char[] ans, int i, int j) {
+        if (si >= ei) {
+            if (si == ei)
+                ans[i] = str.charAt(si);
+            for (char ch : ans)
+                System.out.print(ch);
+            System.out.println();
+            return;
+        }
+
+        if (str.charAt(si) == str.charAt(ei)) {
+            ans[i] = ans[j] = str.charAt(si);
+            printLPS(str, si + 1, ei - 1, dp, ans, i + 1, j - 1);
+        } else if (dp[si + 1][ei] > dp[si][ei - 1]) {
+            // because, here we only took max out of two, ans is not formed here
+            // so can't add anything in our char[] ans.
+            printLPS(str, si + 1, ei, dp, ans, i, j);
+        } else if (dp[si + 1][ei] < dp[si][ei - 1]) {
+            printLPS(str, si, ei - 1, dp, ans, i, j);
+        } else {
+            printLPS(str, si + 1, ei, dp, ans, i, j);
+            printLPS(str, si, ei - 1, dp, ans, i, j);
+        }
+    }
+
+    // for all subsequences, but without duplicacy, take a Hashset or Hashmap
+
+    /************************************************************************************/
+    // longest common subsequence
+
+    public static void solve2() {
+
+        // 1. ans - 3 (ADH)
+        // String s1 = "ABCDGH";
+        // String s2 = "AEDFHR";
+
+        // 2. ans - 4 (GTAB)
+        String s1 = "AGGTAB";
+        String s2 = "GXTXAYB";
+
+        // System.out.println(lcs_rec(s1, s2, 0, 0));
+
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        // System.out.println(lcs_mem(s1, s2, 0, 0, dp));
+        // System.out.println(lcsBackwards_mem(s1, s2, n, m, dp));
+        System.out.println(lcs_dp(s1, s2, n, m, dp));
+        print2d(dp);
+    }
+
+    public static int lcs_rec(String s1, String s2, int i, int j) {
+        int n = s1.length();
+        int m = s2.length();
+        if (i == n || j == m)
+            return 0;
+
+        if (s1.charAt(i) == s2.charAt(j)) {
+            return lcs_rec(s1, s2, i + 1, j + 1) + 1;
+        } else
+            return Math.max(lcs_rec(s1, s2, i + 1, j), lcs_rec(s1, s2, i, j + 1));
+    }
+
+    // when removing character from front
+    public static int lcs_mem(String s1, String s2, int i, int j, int[][] dp) {
+        int n = s1.length();
+        int m = s2.length();
+        if (i == n || j == m)
+            return dp[i][j] = 0;
+
+        if (dp[i][j] != 0)
+            return dp[i][j];
+
+        if (s1.charAt(i) == s2.charAt(j)) {
+            return dp[i][j] = lcs_mem(s1, s2, i + 1, j + 1, dp) + 1;
+        } else
+            return dp[i][j] = Math.max(lcs_mem(s1, s2, i + 1, j, dp), lcs_mem(s1, s2, i, j + 1, dp));
+    }
+
+    public static int lcsBackwards_mem(String s1, String s2, int i, int j, int[][] dp) {
+        if (i == 0 || j == 0)
+            return dp[i][j] = 0;
+
+        if (dp[i][j] != 0)
+            return dp[i][j];
+
+        // here, remember, dp size 1 extra, so string index is 1 less.
+        if (s1.charAt(i - 1) == s2.charAt(j - 1))
+            return dp[i][j] = lcsBackwards_mem(s1, s2, i - 1, j - 1, dp) + 1;
+        else
+            return dp[i][j] = Math.max(lcsBackwards_mem(s1, s2, i - 1, j, dp), lcsBackwards_mem(s1, s2, i, j - 1, dp));
+    }
+
+    // from front 
+    public static int lcs_dp(String s1, String s2, int i, int j, int[][] dp) {
+        for ( ; i>=0; i--) {
+            for (j = s2.length();j>=0;  j--) {
+                int n = s1.length();
+                int m = s2.length();
+                if (i == n || j == m){
+                    dp[i][j] = 0;
+                    continue;
+                }
+
+                if (s1.charAt(i) == s2.charAt(j)) {
+                     dp[i][j] = dp[ i + 1][j + 1] + 1;
+                } else
+                     dp[i][j] = Math.max(dp[ i + 1][j], dp[ i][j + 1]);
+            }
+        }
+        return dp[0][0];
+    }
+
+    /************************************************************************************/
+
+    public static void solve3(){
+
+    }
+
+    
 
 
     /************************************************************************************/
+
 }
--- rajneeshBhaiya/cycle3/DP2/l002/rough.java
+++ rajneeshBhaiya/cycle3/DP2/l002/rough.java
@@ -2,3 +2,13 @@ public class rough {
     
 }
 
+public static int lCSS_02(String s1,String s2,int n,int m,int[][] dp){
+    if(n == 0 || m == 0){
+        return dp[i][j] = 0;
+    }
+
+    if(dp[i][j] != 0) return dp[i][j];
+
+    if(s1.charAt(n-1) == s2.charAt(m-1)) return dp[i][j] = lCSS(s1,s2,n-1,m-1,dp) + 1;
+    else return dp[i][j] = Math.max(lCSS(s1,s2,n-1,j,dp),lCSS(s1,s2,i,m-1,dp));        
+}
